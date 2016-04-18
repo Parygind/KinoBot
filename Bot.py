fd436637abae33e20a28b73615f8158c02565349
parygind <parygind@gmail.com>
2016-04-18 12:45:10 +0600

    Bot

---
+++ Bot.py
@@ -0,0 +1,19 @@
+import telebot
+
+API_TOKEN = '202773259:AAFhv-bdqhAyqd1CZ-j9daHIeW-HeY1QqWM'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+)) Я бот ))\
+""")
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
