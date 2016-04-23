86e497fa7d71aa96f61ebb8782a563beb42daae4
Parygind <parygind@gmail.com>
2016-04-23 23:02:57 +0600

    Tavo2

--- Bot.py
+++ Bot.py
@@ -4,12 +4,10 @@ API_TOKEN = '202773259:AAFhv-bdqhAyqd1CZ-j9daHIeW-HeY1QqWM'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-# Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, "TAVO")
+    bot.reply_to(message, 'TAVO')
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     bot.reply_to(message, message.text)
